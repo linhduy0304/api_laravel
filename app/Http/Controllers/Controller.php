d545d262193a409c3a0da0dd7fd31ede173bf9c0
Le Linh Duy <duyle@techkids.io>
2021-11-17 21:56:23 +0700

    init

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+use App\Models\User;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    
+    function getUser() {
+        
+            $user = User::orderBy('name', 'desc')->get();
+            return $user;
+        
+    }
+
+}
